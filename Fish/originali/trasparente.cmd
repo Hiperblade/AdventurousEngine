768e0826b90f352d403522c3f05172b394385f9b
Giorgio Amadei <hiperblade.2501@gmail.com>
2014-01-30 18:34:45 +0100

    Gif

--- Fish/originali/trasparente.cmd
+++ Fish/originali/trasparente.cmd
@@ -1,2 +1,3 @@
 cd C:\Users\gi.amadei\Documents\GitHub\AdventurousEngine\Fish\originali
 FOR %%G IN (*.png) DO convert %%G -transparent #0fbfa7 ..\%%G
+FOR %%G IN (*.gif) DO convert %%G -transparent #0fbfa7 ..\%%G
